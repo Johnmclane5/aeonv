77bdefb226226c0c81ad7f9d4d471a72979665c3
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-01-04 15:33:29 +0530

    Update aeon_utils.py

--- bot/helper/ext_utils/aeon_utils.py
+++ bot/helper/ext_utils/aeon_utils.py
@@ -1,5 +1,5 @@
 import re
-import pyshorteners
+from pyshorteners import Shortener
 from bot import LOGGER, config_dict
 from re import IGNORECASE, search, escape
 
@@ -17,8 +17,8 @@ async def extract_movie_info(caption):
     return None, None
 
 async def get_movie_poster(movie_name, release_year):
-    TMDB_API = config_dict['TMDB_API_KEY']
-    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API}&query={movie_name}'
+    TMDB_API_KEY = '0dfbeb8ce49d198fb0bf99e08b6a8557'
+    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={movie_name}'
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(tmdb_search_url) as search_response:
@@ -40,7 +40,7 @@ async def get_movie_poster(movie_name, release_year):
                         movie_id = result['id']
                         media_type = result['media_type']
 
-                        tmdb_movie_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API}&language=en-US&include_image_language=en'
+                        tmdb_movie_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&language=en-US&include_image_language=en'
 
                         async with session.get(tmdb_movie_url) as movie_response:
                             movie_data = await movie_response.json()
@@ -51,8 +51,13 @@ async def get_movie_poster(movie_name, release_year):
                             backdrop_path = movie_data['backdrops'][0]['file_path']
                         elif 'backdrop_path' in result and result['backdrop_path']:
                             backdrop_path = result['backdrop_path']
-                            
-                        if backdrop_path:
+
+                        # If both backdrop_path and poster_path are not available, use poster_path
+                        if not backdrop_path and 'poster_path' in result and result['poster_path']:
+                            poster_path = result['poster_path']
+                            poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
+                            return poster_url
+                        elif backdrop_path:
                             backdrop_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
                             return backdrop_url
                         else:
@@ -71,11 +76,10 @@ async def get_movie_poster(movie_name, release_year):
 
     return None
 
-
 def tinyfy(long_url):
-    s = pyshorteners.Shortener()
+    s = Shortener()
     try:
-        short_url = s.osdb.short(long_url)
+        short_url = s.tinyurl.short(long_url)
         LOGGER.info(f'tinyfied {long_url} to {short_url}')
         return short_url
     except Exception:
