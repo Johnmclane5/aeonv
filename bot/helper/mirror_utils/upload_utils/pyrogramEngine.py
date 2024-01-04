d340697387367310c930fb0d13f6b505c83768d9
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-01-04 06:09:48 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -23,6 +23,7 @@ from bot.helper.telegram_helper.message_utils import sendCustomMsg, sendMultiMes
 from bot.helper.ext_utils.fs_utils import clean_unwanted, is_archive, get_base_name
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, sync_to_async, is_telegram_link, is_url, download_image_url
 from bot.helper.ext_utils.leech_utils import get_media_info, get_document_type, take_ss, get_ss, get_mediainfo_link, format_filename, get_audio_thumb
+from bot.helper.ext_utils.aeon_utils import extract_movie_info, get_movie_poster
 
 LOGGER = getLogger(__name__)
 getLogger("pyrogram").setLevel(ERROR)
@@ -359,8 +360,8 @@ class TgUploader:
             movie_name, release_year = await extract_movie_info(file_name)
             tmdb_poster_url = await get_movie_poster(movie_name, release_year)
             LOGGER.info("Got the poster")
-            if tmdb_poster_url:
-                thumb = await self.get_custom_thumb(tmdb_poster_url)
+            if self.__leech_utils['thumb']:
+                thumb = await self.get_custom_thumb(self.__leech_utils['thumb'])
             if not is_image and thumb is None:
                 file_name = ospath.splitext(file)[0]
                 thumb_path = f"{self.__path}/yt-dlp-thumb/{file_name}.jpg"
@@ -401,7 +402,7 @@ class TgUploader:
                 key = 'videos'
                 duration = (await get_media_info(self.__up_path))[0]
                 if thumb is None:
-                    thumb = await take_ss(self.__up_path, duration)
+                    thumb = await self.get_custom_thumb(tmdb_poster_url)
                 if thumb is not None:
                     with Image.open(thumb) as img:
                         width, height = img.size
@@ -514,76 +515,3 @@ class TgUploader:
         self.__is_cancelled = True
         LOGGER.info(f"Cancelling Upload: {self.name}")
         await self.__listener.onUploadError('Cancelled by user!')
-
-async def extract_movie_info(caption):
-    try:
-        regex = re.compile(r'(.+?)(\d{4})')
-        match = regex.search(caption)
-
-        if match:
-             movie_name = match.group(1).replace('.', ' ').strip()
-             release_year = match.group(2)
-             return movie_name, release_year
-    except Exception as e:
-        print(e)
-    return None, None
-
-async def get_movie_poster(movie_name, release_year):
-    TMDB_API_KEY = '0dfbeb8ce49d198fb0bf99e08b6a8557'
-    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={movie_name}'
-    try:
-        async with aiohttp.ClientSession() as session:
-            async with session.get(tmdb_search_url) as search_response:
-                search_data = await search_response.json()
-
-                if search_data['results']:
-                    # Filter results based on release year and first air date
-                    matching_results = [
-                        result for result in search_data['results']
-                        if ('release_date' in result and result['release_date'][:4] == str(release_year)) or
-                        ('first_air_date' in result and result['first_air_date'][:4] == str(
-                            release_year))
-                    ]
-
-                    if matching_results:
-                        result = matching_results[0]
-
-                        # Fetch additional details using movie ID
-                        movie_id = result['id']
-                        media_type = result['media_type']
-
-                        tmdb_movie_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&language=en-US&include_image_language=en'
-
-                        async with session.get(tmdb_movie_url) as movie_response:
-                            movie_data = await movie_response.json()
-
-                        # Use the first backdrop image path from either detailed data or result
-                        backdrop_path = None
-                        if 'backdrops' in movie_data and movie_data['backdrops']:
-                            backdrop_path = movie_data['backdrops'][0]['file_path']
-                        elif 'backdrop_path' in result and result['backdrop_path']:
-                            backdrop_path = result['backdrop_path']
-
-                        # If both backdrop_path and poster_path are not available, use poster_path
-                        if not backdrop_path and 'poster_path' in result and result['poster_path']:
-                            poster_path = result['poster_path']
-                            poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
-                            return poster_url
-                        elif backdrop_path:
-                            backdrop_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
-                            return backdrop_url
-                        else:
-                            print(
-                                "Failed to obtain backdrop and poster paths from movie_data and result")
-
-                    else:
-                        print(
-                            f"No matching results found for movie: {movie_name} ({release_year})")
-
-                else:
-                    print(f"No results found for movie: {movie_name}")
-
-    except Exception as e:
-        print(f"Error fetching TMDB data: {e}")
-
-    return None
