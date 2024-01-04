a99badebce5356763914b1b73b807ab1dc7c713f
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-01-04 15:38:28 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -23,7 +23,6 @@ from bot.helper.telegram_helper.message_utils import sendCustomMsg, sendMultiMes
 from bot.helper.ext_utils.fs_utils import clean_unwanted, is_archive, get_base_name
 from bot.helper.ext_utils.bot_utils import get_readable_file_size, sync_to_async, is_telegram_link, is_url, download_image_url
 from bot.helper.ext_utils.leech_utils import get_media_info, get_document_type, take_ss, get_ss, get_mediainfo_link, format_filename, get_audio_thumb
-from bot.helper.ext_utils.aeon_utils import extract_movie_info, get_movie_poster
 
 LOGGER = getLogger(__name__)
 getLogger("pyrogram").setLevel(ERROR)
@@ -516,3 +515,75 @@ class TgUploader:
         LOGGER.info(f"Cancelling Upload: {self.name}")
         await self.__listener.onUploadError('Cancelled by user!')
 
+async def extract_movie_info(caption):
+    try:
+        regex = re.compile(r'(.+?)(\d{4})')
+        match = regex.search(caption)
+
+        if match:
+             movie_name = match.group(1).replace('.', ' ').strip()
+             release_year = match.group(2)
+             return movie_name, release_year
+    except Exception as e:
+        print(e)
+    return None, None
+
+async def get_movie_poster(movie_name, release_year):
+    TMDB_API_KEY = '0dfbeb8ce49d198fb0bf99e08b6a8557'
+    tmdb_search_url = f'https://api.themoviedb.org/3/search/multi?api_key={TMDB_API_KEY}&query={movie_name}'
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(tmdb_search_url) as search_response:
+                search_data = await search_response.json()
+
+                if search_data['results']:
+                    # Filter results based on release year and first air date
+                    matching_results = [
+                        result for result in search_data['results']
+                        if ('release_date' in result and result['release_date'][:4] == str(release_year)) or
+                        ('first_air_date' in result and result['first_air_date'][:4] == str(
+                            release_year))
+                    ]
+
+                    if matching_results:
+                        result = matching_results[0]
+
+                        # Fetch additional details using movie ID
+                        movie_id = result['id']
+                        media_type = result['media_type']
+
+                        tmdb_movie_url = f'https://api.themoviedb.org/3/{media_type}/{movie_id}/images?api_key={TMDB_API_KEY}&language=en-US&include_image_language=en'
+
+                        async with session.get(tmdb_movie_url) as movie_response:
+                            movie_data = await movie_response.json()
+
+                        # Use the first backdrop image path from either detailed data or result
+                        backdrop_path = None
+                        if 'backdrops' in movie_data and movie_data['backdrops']:
+                            backdrop_path = movie_data['backdrops'][0]['file_path']
+                        elif 'backdrop_path' in result and result['backdrop_path']:
+                            backdrop_path = result['backdrop_path']
+
+                        # If both backdrop_path and poster_path are not available, use poster_path
+                        if not backdrop_path and 'poster_path' in result and result['poster_path']:
+                            poster_path = result['poster_path']
+                            poster_url = f"https://image.tmdb.org/t/p/original{poster_path}"
+                            return poster_url
+                        elif backdrop_path:
+                            backdrop_url = f"https://image.tmdb.org/t/p/original{backdrop_path}"
+                            return backdrop_url
+                        else:
+                            print(
+                                "Failed to obtain backdrop and poster paths from movie_data and result")
+
+                    else:
+                        print(
+                            f"No matching results found for movie: {movie_name} ({release_year})")
+
+                else:
+                    print(f"No results found for movie: {movie_name}")
+
+    except Exception as e:
+        print(f"Error fetching TMDB data: {e}")
+
+    return None
